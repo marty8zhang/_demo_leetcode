40c221affdf246620dbb0d9db8dea09b56f26545
Marty Zhang <marty8zhang@gmail.com>
2024-08-26 17:27:51 +1000

    Improve the `2.1` solution.

--- src/2_add-two-numbers/2.1_recursive-calls.js
+++ src/2_add-two-numbers/2.1_recursive-calls.js
@@ -1,18 +1,15 @@
 import ListNode from './list-node.js';
 
+/*
+ * This code has smells in the sense that only the returned value from the outermost call will be
+ * used, whereas the returned values from the deeper recursive calls will be ignored when the
+ * passed-in (hidden) third parameter will keep being mutated.
+ */
 function addTwoNumbers(l1, l2, ...args) {
   const [prevNode] = args;
   const sum = (l1?.val ?? 0) + (l2?.val ?? 0) + (prevNode?.next?.val ?? 0);
   const l1Next = l1?.next ?? null;
   const l2Next = l2?.next ?? null;
-
-  if (sum === 0 && l1Next === null && l2Next === null) {
-    if (prevNode) {
-      return null;
-    }
-
-    return new ListNode(sum);
-  }
 
   let node;
   if (sum >= 10) {
@@ -22,19 +19,15 @@ function addTwoNumbers(l1, l2, ...args) {
     node = new ListNode(sum);
   }
 
-  if (prevNode) {
+  if ((sum || l1Next || l2Next) && prevNode) {
     prevNode.next = node;
   }
 
-  if (l1Next !== null || l2Next !== null) {
+  if (l1Next || l2Next) {
     addTwoNumbers(l1Next, l2Next, node);
   }
 
-  if (!prevNode) {
-    return node;
-  }
-
-  return null;
+  return node;
 }
 
 export default addTwoNumbers;
